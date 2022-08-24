bf55d1ca8fff7e716c8bdffb5fd1c0bc4ef7cbcf
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-24 18:02:29 -0300

    feat: add tailwind signin page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from 'next/document';
+
+  class MyDocument extends Document {
+    static async getInitialProps(
+      ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+      const initialProps = await Document.getInitialProps(ctx)
+  
+      return initialProps
+    }
+
+  render() {
+    return (
+      <Html className="h-full bg-gray-50">
+        <Head/>
+        <body className="h-full">
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+
